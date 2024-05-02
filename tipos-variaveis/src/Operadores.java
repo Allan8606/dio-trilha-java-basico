2c257bccf0675222f8dc309ffe3fc4ba2a0e0aea
Allan8606 <allan-musico@hotmail.com>
2024-05-02 07:10:23 -0300

    Fazendo alterações na claase TiposVariaveis, e criando a classe Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,42 @@
+public class Operadores {
+  public static void main(String[] args) {
+      //     //classe Operadores.java
+      // int numero = 5;
+          
+      // //Imprimindo o numero negativo
+      // //nao afeta a variavel principal, só vai ser negativo se for alterado para negativo
+      // // numero = - numero;
+      // System.out.println(- numero);
+
+      // //incrementando numero em mais 1 numero, imprime 6
+      // numero ++;
+      // System.out.println(numero);
+
+      // //incrementando numero em mais 1 numero, imprime 7
+      // System.out.println(numero ++);// ops algo de errado não está certo
+
+      // System.out.println(numero);// agora sim, numero virou 7
+
+      // //ordem de precedencia conta aqui
+      // System.out.println(++ numero);
+
+      // boolean verdadeiro = true;
+
+      // System.out.println("Inverteu " + !verdadeiro);
+
+
+
+
+      //INCREMENTO e DECREMENTO 
+
+      int numero = 3;
+      numero ++; //que é abreviacao de-> numero + 1
+
+      // do mesmo jeito posso quando quero decrementar 
+      numero --;
+     
+      System.out.println(numero);
+    
+  }
+  
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,24 @@
 public class TiposVariaveis {
 
+  public static void main(String[] args) {
+
+    String nome = "Meu Nome";
+
+
+
+    double salario = 1500;
+    byte valor = 2;
+
+  
+
+      short numeroCurto = 1;
+      int numeroNormal = numeroCurto;
+      short numeroCurto2 = (short) numeroNormal;
+
+      // Constantes: deve vim escrita da palavra final antes, 
+      //e de preferencia que seja nomeada com letras maiusculas 
+      final double VALOR_DE_PI = 3.14;
+
+  }
+ 
 }
